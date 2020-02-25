71469a88ce316d45d26a712000a29f2de746e15c
github username <meliajm@gmail.com>
2020-02-24 19:04:23 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "studentss", force: :cascade do |t|
+  create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
   end
